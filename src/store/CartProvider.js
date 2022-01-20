6fc2ff7bfc0e5857a550a57faf0f713eedb9000f
Pannu786 <thepannu786@gmail.com>
2022-01-20 22:37:14 +0530

    Cart display single item display logic dne

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -9,14 +9,33 @@ const defaultCartState = {
 
 const cartReducer = (state, action) => {
   if (action.type === 'ADD_ITEM') {
-    const updateItems = [...state.items, action.item];
     const updateTotalAmount =
       state.totalAmount + action.item.price * action.item.amount;
+
+    const existingCartItemIndex = state.items.findIndex(
+      (item) => item.id === action.item.id
+    ); // if this item is already in the cart, we don't want to add it again
+
+    const existingCartItem = state.items[existingCartItemIndex];
+    let updatedItems;
+
+    if (existingCartItem) {
+      const updatedItem = {
+        ...existingCartItem,
+        amount: existingCartItem.amount + action.item.amount,
+      };
+      updatedItems = [...state.items];
+      updatedItems[existingCartItemIndex] = updatedItem;
+    } else {
+      updatedItems = [...state.items, action.item];
+    }
+
     return {
-      items: updateItems,
+      items: updatedItems,
       totalAmount: updateTotalAmount,
     };
   }
+
   return defaultCartState;
 };
 
