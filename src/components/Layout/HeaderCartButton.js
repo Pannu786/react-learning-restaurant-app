6f8b430a8505167887ad251d79dd10ecd237698a
Pannu786 <thepannu786@gmail.com>
2022-01-21 21:29:48 +0530

    Popup cart effect dne with useEffect

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,18 +1,38 @@
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 
 import CartContext from '../../store/cart-context';
 import CartIcon from '../Cart/CartIcon';
 import styles from './HeaderCartButton.module.css';
 
 const HeaderCartButton = (props) => {
+  const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
   const cartCtx = useContext(CartContext);
 
-  const numberOfCartItems = cartCtx.items.reduce((currentnum, item) => {
+  const { items } = cartCtx;
+
+  const numberOfCartItems = items.reduce((currentnum, item) => {
     return currentnum + item.amount;
   }, 0);
 
+  const btnStyels = `${styles.button} ${btnIsHighlighted ? styles.bump : ''}`;
+
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+    setBtnIsHighlighted(true);
+
+    const timer = setTimeout(() => {
+      setBtnIsHighlighted(false);
+    }, 300)
+
+    return () => {
+      clearTimeout(timer)
+    }
+  }, [items]);
+
   return (
-    <button className={styles.button} onClick={props.onClick}>
+    <button className={btnStyels} onClick={props.onClick}>
       <span className={styles.icon}>
         <CartIcon />
       </span>
